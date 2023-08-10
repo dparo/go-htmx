306bcfac54992cd91ae102e232400c2b7adf91e1
Davide Paro <dparo@outlook.it>
2023-08-10 23:33:11 +0200

    progress

--- routes.go
+++ routes.go
@@ -2,11 +2,18 @@ package main
 
 import "net/http"
 
+type Cmd struct {
+	Pid  int
+	Argv string
+}
+
 func index_route(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("HX-Trigger", "evProcListRefresh")
 
 	params := map[string]interface{}{
-		"Name": "Davide",
+		"Name":    "Davide",
+		"Cmds":    []Cmd{{0, "systemd"}, {1, "foo"}},
+		"Signals": []string{"SIGTERM", "SIGINT", "SIGKILL"},
 	}
 	ExecuteTemplate("index.gohtml", w, params)
 }
